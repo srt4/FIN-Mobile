0cc9933f874ba5674652f138ff46b2fac871a14f
Spencer Thomas <spencer@spencer-laptop.(none)>
2011-04-18 20:26:56 -0700

    Fixed map resizing the entire website - experimenting with tabs on homepage

--- r/addListeners.js
+++ r/addListeners.js
@@ -5,8 +5,10 @@ var currLoc = { lat: -1 , lon: -1};
 $(document).ready(function(){
      // stop the map pages from caching
      $("div[data-role*='page']").live('pagehide', function(event, ui) {
-            if ($(this).children("div[data-role*='content']").is(".command-no-cache"))
+            if ($(this).children("div[data-role*='content']").is(".command-no-cache")) {
                 $(this).remove();
+                $('[data-role=content]').height();
+            }
     });
     // add some placeholder links; hopefully these get amended later
     $('a').attr("href", function(i, href) {
